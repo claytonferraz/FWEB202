b42e7124145621477ae2cb4665cd8a30b1a12350
clayton <clayton.andrade@ifro.edu.br>
2020-09-17 21:31:13 -0400

    Aula Java Script

---
+++ clayton/scripts/hello.js
@@ -0,0 +1,16 @@
+//alert("Olá Mundo");
+var nome = "Clayton Ferraz";
+var valor = 234;
+var nomes = [23,"Jose","Tati","Maicon"];
+//alert(nomes[0]);
+//alert(nomes[1]);
+
+var carro = {
+marca: "Ford",
+modelo: "Fiesta"
+
+
+}
+carro.modelo = "Ranger"
+alert(carro.modelo);
+confirm

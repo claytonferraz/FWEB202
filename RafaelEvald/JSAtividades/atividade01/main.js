a367b06705396d335a3453d81ebdd2c0b980b6b5
Rafael <rafael-ev-silva@outlook.com>
2020-09-20 00:16:13 -0400

    Atividades do JS

---
+++ RafaelEvald/JSAtividades/atividade01/main.js
@@ -0,0 +1,12 @@
+function NumImpar() {
+  let num = document.querySelector("#resut");
+  let numImpar = {};
+
+  for (n = 0; n <= 10; n++){
+    if(n / 2 === 1) {
+      numImpar.push(n);
+    }
+  }
+
+
+}

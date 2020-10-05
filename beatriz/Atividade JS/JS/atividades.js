59b22601539a51a61149e2f027027fc8c0c4200e
Beatriz Oliveira <beatriz100143@gmail.com>
2020-10-04 23:49:01 -0400

    Atividade JS
    Foi oque deu pra fazer

--- beatriz/Atividade JS/JS/atividades.js
+++ beatriz/Atividade JS/JS/atividades.js
@@ -37,7 +37,7 @@ function ContarNomeAtv04() {
                 [form.prinome.value, 
                 FormAtv4.segnome.value, 
                 FormAtv4.tercnome.value, 
-                FormAtv4.quartnome.value, 
+                FormAtv4.quartnome.value,  
                 FormAtv4.quintnome.value, 
                 FormAtv4.sextnome.value, 
                 FormAtv4.setnome.value, 
@@ -48,9 +48,104 @@ function ContarNomeAtv04() {
                 {
                     if (nomes4letras[i].length == 4) 
                     {
-                    document.getElementById('resultAtv4').innerHTML = "O(s) Nome(s) com a(s) quatro letra(s) são: "+nomes4letras[i];
+                        document.getElementById('resultAtv4').innerHTML = "O(s) Nome(s) com a(s) quatro letra(s) são: "+nomes4letras[i];
                     }
                 }
             
         
+    }
+
+
+    function ValidacaoDadosFormularioAtv5() {
+        var FormAtv5 = document.getElementById('form03');
+        if (FormAtv5.nome.value == "") {
+            alert("Campo PRIMEIRO NOME em branco!.");
+            return false;
+        }
+    
+        if (FormAtv5.segundonome.value == "") {
+            alert("Campo SOBRENOME em branco!.");
+            return false;
+        }
+    
+        if (FormAtv5.datanasc.value == "") {
+            alert("Campo DATA DE NASCIMENTO em branco!.");
+            return false;
+        }
+    
+        var escolhaSexo = -1;
+        for(var i = FormAtv5.sexo.length - 1; i > -1; i--) {
+            if(FormAtv5.sexo[i].checked) {
+                escolhaSexo = i;
+            }
+        }
+        if (escolhaSexo == -1) {
+            alert("Campo SEXO em branco!!");
+            return false;
+        }
+    
+        if (FormAtv5.rua.value == "") {
+            alert("Campo RUA em branco!.");
+            return false;
+        }
+    
+        if (FormAtv5.numero.value == "") {
+            alert("Campo NUMERO em branco!.");
+            return false;
+        }
+    
+        if (FormAtv5.bairro.value == "") {
+            alert("Campo BAIRRO em branco!.");
+            return false;
+        }
+    
+        if (FormAtv5.cidade.value == "") {
+            alert("Campo CIDADE em branco! ");
+            return false;
+        }
+    
+        if (FormAtv5.estado.value == "") {
+            alert("Campo CIDADE em branco!. ");
+            return false;
+        }
+    }
+    
+    
+
+    function ValidacaoDadosAtv06() {
+        var FormAtv6 = document.getElementById('form04');
+    
+        if (FormAtv6.user.value == "") {
+            alert("Digite o seu usuário.");
+            return false;
+        }
+    
+        if (FormAtv6.senha1.value == "") {
+            alert("Digite a sua senha.");
+            return false;
+        }
+    
+        if (FormAtv6.senha2.value == "") {
+            alert("Confirme a sua senha.");
+            return false;
+        }
+    
+        if (FormAtv6.senha1.value != FormAtv6.senha2.value) {
+            alert("Desculpe, as senhas digitas são diferentes, tente novamente.");
+            return false;
+        }
+    }
+    
+    function ValidaCPFeEmailAtv07() {
+        var FormAtv7 = document.getElementById('FormularioAtiv7');
+    
+        if(FormAtv7.email.value.indexOf("@") == -1 || FormAtv7.email.value.indexOf(".") == -1) {
+            alert("Insira um email válido");
+            return false;
+        }
+        
+        if(FormAtv7.cpf.value == "") {
+            alert("Insira seu CPF!");
+            return false;
+        }
     }

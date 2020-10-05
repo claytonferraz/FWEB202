527e4eb017a797e284f0ac2e8a3ba5d202d3b7db
luana cabral <luanacabral430@gmail.com>
2020-10-04 23:55:41 -0400

    ATIVIDADE03JS(LUANA)

---
+++ Luana/APRENDENDOJS/atividade03/formu.js
@@ -0,0 +1,18 @@
+
+       function somar() {
+        var numero1 = parseInt(document.getElementById('numero1').value);
+        var numero2 = parseInt(document.getElementById('numero2').value);     
+        var numero3 = parseInt(document.getElementById('numero3').value);   
+        var numero4 = parseInt(document.getElementById('numero4').value);   
+    	var soma = numero1 + numero2 + numero3 + numero4 ;
+        alert("numero 1 = "+ numero1+" | numero 2 = "+numero2+ "numero 3 =  " + numero3+ "numero 4 ="+ numero4 + "       soma = "+ soma);
+         
+       }
+
+      
+         
+         
+         
+         
+
+        
